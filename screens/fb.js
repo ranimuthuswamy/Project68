4e343770cb6610caafbae9dd4a13a2157015a6c4
ranimuthuswamy <67144212+ranimuthuswamy@users.noreply.github.com>
2020-11-21 22:00:19 +0530

    Added Header Component

--- screens/fb.js
+++ screens/fb.js
@@ -1,20 +1,35 @@
 import * as React from "react";
 import { View, Text } from "react-native";
-
+import { Header } from "react-native-elements";
 export default class Facebook extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text
+      <View style={{ flex: 1 }}>
+        <Header
+          backgroundColor={"#E60576"}
+          centerComponent={{
+            text: "Buzz App",
+            style: { fontSize: 28, color: "white" },
+          }}
+        />
+        <View
           style={{
-            fontSize: 20,
-            textAlign: "center",
-            color: "blue",
-            fontWeight: "bold",
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center"
           }}
         >
-          FACEBOOK
-        </Text>
+          <Text
+            style={{
+              fontSize: 20,
+              textAlign: "center",
+              color: "blue",
+              fontWeight: "bold",
+            }}
+          >
+            FACEBOOK
+          </Text>
+        </View>
       </View>
     );
   }
